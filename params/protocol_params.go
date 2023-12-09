88464d72ed01a24d0bc0e9fed4f11669669f4dac
bilgin-kocak <kocakbilgin@gmail.com>
2023-12-09 13:36:42 +0300

    feat: protocol gas fees added for poseidon

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -142,6 +142,8 @@ const (
 	Ripemd160PerWordGas uint64 = 120  // Per-word price for a RIPEMD160 operation
 	IdentityBaseGas     uint64 = 15   // Base price for a data copy operation
 	IdentityPerWordGas  uint64 = 3    // Per-work price for a data copy operation
+	PoseidonBaseGas       uint64 = 15   // Base price for a SHA256 operation
+	PoseidonPerWordGas    uint64 = 3   // Per-word price for a SHA256 operation
 
 	Bn256AddGasByzantium             uint64 = 500    // Byzantium gas needed for an elliptic curve addition
 	Bn256AddGasIstanbul              uint64 = 150    // Gas needed for an elliptic curve addition
